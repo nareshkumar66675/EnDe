e0621e91f301cc74a0bc27bf60f7c219af49762b
Naresh Kumar <naresh.sampath@scientificgames.com>
2017-05-30 10:41:52 +0530

    Initial Commit
    EnDe - ENcryptionDEcrytpion Tool

---
+++ EnDe/FileCryptoService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EnDe
+{
+    public static class FileCryptoService
+    {
+        private const string IV = "NARESHKU";
+
+        public static void EncryptFile(string sInputFilename,string sOutputFilename,string sKey)
+        {
+            try
+            {
+                FileStream fsInput = new FileStream(sInputFilename, FileMode.Open, FileAccess.Read);
+                FileStream fsEncrypted = new FileStream(sOutputFilename, FileMode.Create, FileAccess.Write);
+                TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+                PasswordDeriveBytes pwd = new PasswordDeriveBytes(sKey, Encoding.ASCII.GetBytes(IV), "SHA1", 2);
+                //Rfc2898DeriveBytes k2 = new Rfc2898DeriveBytes(sKey.ToString(), Encoding.ASCII.GetBytes(IV));
+                Console.WriteLine("Encrytping file:" + Path.GetFileNameWithoutExtension(sInputFilename));
+                DES.Key = pwd.GetBytes(16);
+                DES.IV = pwd.GetBytes(8); //Encoding.ASCII.GetBytes(IV);
+
+
+                ICryptoTransform desencrypt = DES.CreateEncryptor();
+                CryptoStream cryptostream = new CryptoStream(fsEncrypted, desencrypt, CryptoStreamMode.Write);
+
+                byte[] bytearrayinput = new byte[fsInput.Length];
+                fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+                cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+                Console.WriteLine("Encrytping file:" + Path.GetFileNameWithoutExtension(sInputFilename)+"Completed");
+                cryptostream.Close();
+                fsInput.Close();
+                fsEncrypted.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static void DecryptFile(string sInputFilename, string sOutputFilename, string sKey)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+                //Rfc2898DeriveBytes k2 = new Rfc2898DeriveBytes(sKey.ToString(), Encoding.ASCII.GetBytes(IV));
+                PasswordDeriveBytes pwd = new PasswordDeriveBytes(sKey, Encoding.ASCII.GetBytes(IV), "SHA1", 2);
+                //A 64 bit key and IV is required for this provider.
+                //Set secret key For DES algorithm.
+                DES.Key = pwd.GetBytes(16);
+                //Set initialization vector.
+                DES.IV = pwd.GetBytes(8);// Encoding.ASCII.GetBytes(IV);
+                Console.WriteLine("Decrytping file:" + Path.GetFileNameWithoutExtension(sInputFilename));
+                var encryptedBytes = File.ReadAllBytes(sInputFilename);
+                //Create a file stream to read the encrypted file back.
+                MemoryStream fsread = new MemoryStream(File.ReadAllBytes(sInputFilename));
+                //Create a DES decryptor from the DES instance.
+                ICryptoTransform desdecrypt = DES.CreateDecryptor();
+                //Create crypto stream set to read and do a 
+                //DES decryption transform on incoming bytes.
+                CryptoStream cryptostreamDecr = new CryptoStream(fsread, desdecrypt, CryptoStreamMode.Read);
+                //Print the contents of the decrypted file.
+                byte[] plainBytes = new byte[encryptedBytes.Length];
+
+                int DecryptedCount = cryptostreamDecr.Read(plainBytes, 0, plainBytes.Length);
+                File.WriteAllBytes(sOutputFilename, plainBytes);
+                Console.WriteLine("Decrytping file:" + Path.GetFileNameWithoutExtension(sInputFilename)+"Completed");
+                fsread.Close();
+                cryptostreamDecr.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ EnDe/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EnDe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Decrypt Current Directory Files
+                if (args == null || args.Length == 0)
+                {
+                    var allFiles = GetCurrentDirectoryEncryptedFiles();
+                    foreach (var file in allFiles)
+                    {
+                        var key = GetKeyFromUser();
+                        var decryptedFile = Path.ChangeExtension(file, Path.GetExtension(file).Replace("ende", ""));
+
+                        try
+                        {
+                            FileCryptoService.DecryptFile(file, decryptedFile, key);
+                        }
+                        catch (CryptographicException)
+                        {
+
+                            Console.WriteLine("Incorrect Password. Please Try Again.");
+                        }
+                    }
+                }
+                else if (args.Length > 0 && (args[0].ToUpper() == "E" || args[0].ToUpper() == "ENCRYPT"))
+                {
+                    var filePath = GetFilePath(args);
+
+                    var key = GetKeyFromUser();
+                    var encryptedFile = Path.ChangeExtension(filePath, Path.GetExtension(filePath) + "ende");
+                    FileCryptoService.EncryptFile(filePath, encryptedFile, key);
+                }
+                else if (args.Length > 0 && (args[0].ToUpper() == "D" || args[0].ToUpper() == "DECRYPT"))
+                {
+                    string filePath = GetFilePath(args);
+                    var key = GetKeyFromUser();
+
+                    var decryptedFile = Path.ChangeExtension(filePath, Path.GetExtension(filePath).Replace("ende", ""));
+                    try
+                    {
+                        FileCryptoService.DecryptFile(filePath, decryptedFile, key);
+                    }
+                    catch (CryptographicException)
+                    {
+                        Console.WriteLine("Incorrect Password. Please Try Again.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Exception Occured:" + ex.Message);
+            }
+            Console.ReadLine();
+        }
+        private static string GetFilePath(string[] args)
+        {
+            string filePath = string.Empty;
+            if (args.Length == 2)
+            {
+                filePath = args[1];
+                if (!File.Exists(filePath))
+                    filePath = GetFilePathFromUser();
+            }
+            else
+                filePath = GetFilePathFromUser();
+            return filePath;
+
+        }
+        private static List<string> GetCurrentDirectoryEncryptedFiles()
+        {
+            List<string> filesList = new List<string>();
+
+            filesList =Directory.GetFiles(Directory.GetCurrentDirectory(),"*.*ende").ToList();
+
+            return filesList;
+        }
+
+        private static string GetFilePathFromUser()
+        {
+            string path = string.Empty;
+            while(true)
+            {
+                Console.WriteLine("Enter File Path:");
+                path = Console.ReadLine();
+                if (File.Exists(path))
+                    break;
+                else
+                    Console.WriteLine("File Not Found.");
+            }
+            return path;
+        }
+
+        private static string GetKeyFromUser()
+        {
+            StringBuilder passPhrase = new StringBuilder();
+            ConsoleKeyInfo key;
+            Console.WriteLine("Enter Password:");
+            do
+            {
+                key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    // Append the character to the password.
+                    passPhrase.Append(key.KeyChar);
+                    Console.Write("*");
+                }
+                // Exit if Enter key is pressed.
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+            return passPhrase.ToString(); ;
+        }
+    }
+}
